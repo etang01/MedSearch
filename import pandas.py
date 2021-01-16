42e7c3b21832aae221c6850f688b5de84400e87b
EthanTang <62631971+etang01@users.noreply.github.com>
2021-01-16 14:13:50 -0800

    update backend

--- import pandas.py
+++ import pandas.py
@@ -1,3 +1,2 @@
 import pandas as pd 
-df = pandas.read_csv('https://docs.google.com/spreadsheets/d/1a1pUVKaP7IwUWeoqMAIIKnxxv3L75ZMFuAicFiRrd2I/edit?usp=sharing')
-symp.list = pandas.read_csv('')
+import numpy as np 
